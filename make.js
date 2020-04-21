94ee2660f8b79ca3e19ff0c87d0f558fc7a6dc1e
jiwoohong <uzihoon.dev@gmail.com>
2020-04-21 23:19:27 +0900

    initial commit

---
+++ make.js
@@ -0,0 +1,129 @@
+const xslx = require("xlsx");
+const fs = require("fs");
+const _ = require("lodash");
+
+const month = 8;
+
+const ccc = {
+  12: ["SAT", "SUN", "MON", "TUE", "WEN", "THU", "FRI"],
+  11: ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WEN"],
+  10: ["MON", "TUE", "WEN", "THU", "FRI", "SAT", "SUN"],
+  9: ["SAT", "SUN", "MON", "TUE", "WEN", "THU", "FRI"],
+  8: ["WEN", "FRI"]
+};
+const MAX = { 12: 31, 11: 30, 10: 31, 9: 30 };
+
+const exceptList = {
+  12: [25],
+  11: [],
+  10: [3, 9],
+  9: [24, 25, 26]
+};
+const targetDate = [];
+const vv = ccc[month].map((l, i) => {
+  const start = i + 1;
+  const a = [];
+  for (let i = 0; i < 5; i++) {
+    const date = start + i * 7;
+    if (exceptList[month].includes(date) && l !== "SUN") break;
+    if (date > MAX[month]) break;
+    a.push(`2018.${month}.${date}`);
+  }
+
+  if (l === "SUN") {
+    exceptList[month].map(c => {
+      a.push(`2018.${month}.${c}`);
+    });
+  }
+
+  targetDate.push({ key: l, list: a });
+});
+
+const data = xslx.readFile(`./${month}.xlsx`);
+const sheet_name = data.SheetNames;
+const xlData = xslx.utils.sheet_to_json(data.Sheets[sheet_name[0]]);
+
+const line = [];
+
+for (let i = 1; i < 9; i++) {
+  line.push(`${i}호선`);
+}
+
+const time = [
+  "05 ~ 06",
+  "06 ~ 07",
+  "07 ~ 08",
+  "08 ~ 09",
+  "09 ~ 10",
+  "10 ~ 11",
+  "11 ~ 12",
+  "12 ~ 13",
+  "13 ~ 14",
+  "14 ~ 15",
+  "15 ~ 16",
+  "16 ~ 17",
+  "17 ~ 18",
+  "18 ~ 19",
+  "19 ~ 20",
+  "20 ~ 21",
+  "21 ~ 22",
+  "22 ~ 23",
+  "23 ~ 24",
+  "00 ~ 01"
+];
+
+const type = ["승차", "하차"];
+
+const filterData = _.fromPairs(
+  line.map(l => {
+    const filtered = xlData.filter(x => x.line === l); // 1호선 전체 데이터
+    const idList = _.uniq(filtered.map(f => f.id)); // 1호선의 전체 id
+
+    const l_data = _.fromPairs(
+      idList.map(id => {
+        const data = filtered.filter(f => f.id === id); // 1호선에 동대문인 데이터
+
+        const id_data = _.fromPairs(
+          targetDate.map(td => {
+            const tdList = td.list;
+
+            const totalData = _.fromPairs(
+              type.map(ty => {
+                const ty_data = data.filter(d => d.type === ty); // 1호선에 동대문에 승차 데이터;
+
+                const tData = _.fromPairs(
+                  time.map(t => {
+                    let t_data = 0;
+
+                    tdList.map(tl => {
+                      const tlData = ty_data.filter(d => d.date === tl); // 1호선에 동대문에 2018.12.1에 승차;
+                      t_data =
+                        t_data +
+                        tlData.map(tld => +tld[t]).reduce((a, b) => a + b, 0);
+                    });
+                    return [t, Math.round(t_data / tdList.length)];
+                  })
+                );
+                return [ty, tData];
+              })
+            );
+
+            return [td.key, totalData];
+          })
+        );
+
+        return [id, id_data];
+      })
+    );
+
+    return [l, l_data];
+  })
+);
+
+line.map(l => {
+  fs.writeFile(`./${month}/${l}.json`, JSON.stringify(filterData[l]), function(
+    err
+  ) {
+    console.log(l);
+  });
+});
